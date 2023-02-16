fe1e186c4814249b2c98e72249102608ce198403
aniamrstc <ania.mrstc@eduge.ch>
2023-02-16 15:13:22 +0100

    Update M152_AM.sql

--- m152/divers/M152_AM.sql
+++ m152/divers/M152_AM.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : jeu. 02 fév. 2023 à 16:06
+-- Généré le : jeu. 16 fév. 2023 à 15:11
 -- Version du serveur :  10.3.37-MariaDB-0ubuntu0.20.04.1
 -- Version de PHP : 8.1.13
 
@@ -43,8 +43,11 @@ CREATE TABLE `MEDIA` (
 --
 
 INSERT INTO `MEDIA` (`idMedia`, `typeMedia`, `nomMedia`, `creationDate`, `idPost`) VALUES
-(2, 'png', 'frac.png', '2023-02-01 23:00:00', 4),
-(3, 'png', 'frac.png', '2023-02-01 23:00:00', 5);
+(286, 'image/png', 'endGame2_63ee379869fc0.png', '2023-02-16 14:03:04', 256),
+(287, 'image/png', 'Confection_63ee37c3c5bf0.png', '2023-02-16 14:03:47', 258),
+(288, 'image/png', 'endGame2_63ee37c3c9d3b.png', '2023-02-16 14:03:47', 258),
+(289, 'video/mp4', '63e4ffe7482884.80244378_63ee37df60635.mp4', '2023-02-16 14:04:15', 259),
+(290, 'image/jpeg', '1977198-or-argent-et-bronze-trophee-illustrationle-isole-sur-blanc-gratuit-vectoriel_63ee37df60724.jpg', '2023-02-16 14:04:15', 259);
 
 -- --------------------------------------------------------
 
@@ -55,8 +58,8 @@ INSERT INTO `MEDIA` (`idMedia`, `typeMedia`, `nomMedia`, `creationDate`, `idPost
 CREATE TABLE `POST` (
   `idPost` int(11) NOT NULL,
   `commentaire` text NOT NULL,
-  `creationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `modificationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `creationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modificationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -64,11 +67,10 @@ CREATE TABLE `POST` (
 --
 
 INSERT INTO `POST` (`idPost`, `commentaire`, `creationDate`, `modificationDate`) VALUES
-(1, 'RRRR', '2023-02-01 23:00:00', '2023-02-01 23:00:00'),
-(2, 'RRRR', '2023-02-01 23:00:00', '2023-02-01 23:00:00'),
-(3, 'RRRR', '2023-02-01 23:00:00', '2023-02-01 23:00:00'),
-(4, 'RRRR', '2023-02-01 23:00:00', '2023-02-01 23:00:00'),
-(5, 'co', '2023-02-01 23:00:00', '2023-02-01 23:00:00');
+(256, 'test', '2023-02-16 14:03:04', '2023-02-16 14:03:04'),
+(258, 'salut', '2023-02-16 14:03:47', '2023-02-16 14:03:47'),
+(259, 'vidéo', '2023-02-16 14:04:15', '2023-02-16 14:04:15'),
+(260, 'c\'est vide', '2023-02-16 14:05:20', '2023-02-16 14:05:20');
 
 --
 -- Index pour les tables déchargées
@@ -95,13 +97,13 @@ ALTER TABLE `POST`
 -- AUTO_INCREMENT pour la table `MEDIA`
 --
 ALTER TABLE `MEDIA`
-  MODIFY `idMedia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idMedia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=291;
 
 --
 -- AUTO_INCREMENT pour la table `POST`
 --
 ALTER TABLE `POST`
-  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=261;
 
 --
 -- Contraintes pour les tables déchargées
